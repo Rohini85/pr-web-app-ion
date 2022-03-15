a8c35fb22f63b1a19294e70288923dc4f547689e
Rohini85 <99239234+Rohini85@users.noreply.github.com>
2022-03-15 17:26:17 -0500

    Update build-customAMI.sh

--- build-customAMI.sh
+++ build-customAMI.sh
@@ -7,5 +7,5 @@ sudo apt-get update -y
 sudo apt-get install tomcat9-admin tomcat9-common -y
 sudo apt-get install tomcat9 -y
 cd /var/lib/tomcat9/webapps/
-sudo wget https://kumar-test-bucket.s3.us-east-2.amazonaws.com/ion.war
+sudo wget https://my-tf-test-bucket2142020.s3.us-west-2.amazonaws.com/time-tracker-web-0.3.1-SNAPSHOT.war:wq
 sudo systemctl start tomcat9
